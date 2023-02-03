719390025cd2dfe5b7af825d94af6cef88e9c1d7
Alek Novkovski <alek@greatcode2.com>
2023-02-03 21:02:39 +0100

    Attach a testUser boolean to req.user object

--- working/middleware/authentication.js
+++ working/middleware/authentication.js
@@ -13,7 +13,8 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    const testUser = payload.userId === '63dc3a2f7cee4e636883eb4b';
+    req.user = { userId: payload.userId, name: payload.name, testUser }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
