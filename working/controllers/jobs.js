46e0519e76a7e790e0073092f1a44d4e12b1e23b
Alek Novkovski <alek@greatcode2.com>
2023-02-03 20:53:55 +0100

    Add pagination logic

--- working/controllers/jobs.js
+++ working/controllers/jobs.js
@@ -34,8 +34,23 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
-  const jobs = await result
-  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const jobs = await result;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJobs,
+    numOfPages,
+    count: jobs.length
+  })
 }
 const getJob = async (req, res) => {
   const {
